e8dd0e458c726f4a1f70e0a350c9834750183e4b
Zach <zwsaqx@gmail.com>
2022-07-26 11:12:13 +1200

    first commit

---
+++ cise-react-learn/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          YO MY G! make sure to edit <code>src/App.js</code> and save to reload!!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React, or nottt idk
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
